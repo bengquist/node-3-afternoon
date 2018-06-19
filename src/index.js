1e89b27d18cff79283a322a6e585721f1fa347c5
Blake Engquist <engquist_blake@yahoo.com>
2018-06-19 16:29:12 -0500

    completed

--- src/index.js
+++ src/index.js
@@ -1,20 +1,20 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './components/App';
-import registerServiceWorker from './registerServiceWorker';
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./components/App";
+import registerServiceWorker from "./registerServiceWorker";
 
 import { BrowserRouter } from "react-router-dom";
 import { Provider } from "react-redux";
 
-import store from './store';
+import store from "./store";
 
 ReactDOM.render(
   <BrowserRouter>
-    <Provider store={ store }>
+    <Provider store={store}>
       <App />
     </Provider>
-  </BrowserRouter>, 
-  document.getElementById('root')
+  </BrowserRouter>,
+  document.getElementById("root")
 );
 
 registerServiceWorker();
